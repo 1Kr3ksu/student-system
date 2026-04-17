aed943268e263565f165087663b90a9b72af09ec
1Kr3ksu <kacperbielawski33@gmail.com>
2026-04-17 10:03:58 +0200

    feat(sqlstring): add SqlString module for SQL escaping and formatting
    - Introduced SqlString module with methods for escaping identifiers and values.
    - Implemented array and object handling for SQL queries.
    - Added date formatting and buffer handling for SQL strings.
    
    feat(string_decoder): add string_decoder module for Node.js
    
    - Added string_decoder implementation for handling buffer to string conversions.
    - Included support for various encodings including UTF-8 and UTF-16LE.
    - Implemented tests for string decoding functionality.
    
    chore(util-deprecate): add util-deprecate module with browser support
    
    - Implemented deprecation warnings for functions in both Node.js and browser environments.
    - Added configuration options for controlling deprecation behavior via localStorage.

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'student_managment'
+})
+connection.connect()
+
 const app = express()
 const port = 3000
 
+app.use(cors())
+app.use(express.json())
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({ message: "Hello from Backend" })
 })
 
+app.get('/students', (req, res) => {
+  const students = [
+    { id: 1, name: "Jan Kowalski", email: "jan@example.com" },
+    { id: 2, name: " Nowak", email: "anna@example.com" },
+  ]
+  res.json(students)
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server działa na http://localhost:${port}`)
 })
