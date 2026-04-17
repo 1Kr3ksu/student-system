2c184471c39c22f8d35e21ce3973d489b3a4451c
1Kr3ksu <kacperbielawski33@gmail.com>
2026-04-17 10:33:17 +0200

    feat(students): implement student creation endpoint and remove mock data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql')
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -21,12 +22,29 @@ app.get('/', (req, res) => {
   res.json({ message: "Hello from Backend" })
 })
 
+app.post('/students', (req, res) => {
+  console.log("BODY:", req.body);
+
+  const { name, email } = req.body;
+
+  const sql = "INSERT INTO students (full_name, email) VALUES (?, ?)";
+
+  connection.query(sql, [name, email], (err, result) => {
+    if (err) {
+      console.log("MYSQL ERROR:", err);
+      return res.status(500).json({ error: err.message });
+    }
+
+    res.json({ id: result.insertId, name, email });
+  });
+});
 app.get('/students', (req, res) => {
-  const students = [
-    { id: 1, name: "Jan Kowalski", email: "jan@example.com" },
-    { id: 2, name: " Nowak", email: "anna@example.com" },
-  ]
-  res.json(students)
+  connection.query('SELECT full_name , email  FROM students', (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message })
+    }
+    res.json(results)
+  })
 })
 
 
